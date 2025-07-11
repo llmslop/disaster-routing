28cd91c18a784c1c2c6e21627fabc0f5b5ab015b
btmxh <ngoduyanh.chip@gmail.com>
2025-07-12 04:36:11 +0700

    fix(scripts)!: fix `run_all.sh` regex support

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+instances=$(find instances)
+if [ -n "$1" ]; then
+    instances=$(grep -E $1 <<< "$instances")
+fi
 
-uv run -m disaster_routing.main -m instance.path=`ls instances/nsfnet-$@*.json | paste -sd,` +router=greedy,flow,greedy+ls,flow+ls
+uv run -m disaster_routing.main -m instance.path=$(paste -sd, <<< "$instances") +router=greedy,flow,greedy+ls,flow+ls
