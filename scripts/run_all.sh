54c011f2ebb62e55abac8b2cf924cebca6768254
btmxh <ngoduyanh.chip@gmail.com>
2025-07-12 04:37:49 +0700

    fix(scripts): sh compatability

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 instances=$(find instances)
 if [ -n "$1" ]; then
-    instances=$(grep -E $1 <<< "$instances")
+    instances=$(echo "$instances" | grep -E $1)
 fi
 
-uv run -m disaster_routing.main -m instance.path=$(paste -sd, <<< "$instances") +router=greedy,flow,greedy+ls,flow+ls
+uv run -m disaster_routing.main -m instance.path=$(echo "$instances" | paste -sd,) +router=greedy,flow,greedy+ls,flow+ls
