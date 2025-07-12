3bf0c65b2777d363f7503ef97442ddd4b6e9da17
btmxh <ngoduyanh.chip@gmail.com>
2025-07-12 08:49:56 +0700

    chore(scripts): add SGA to the list of routers

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -4,4 +4,4 @@ if [ -n "$1" ]; then
     instances=$(echo "$instances" | grep -E $1)
 fi
 
-uv run -m disaster_routing.main -m instance.path=$(echo "$instances" | paste -sd,) +router=greedy,flow,greedy+ls,flow+ls
+uv run -m disaster_routing.main -m instance.path=$(echo "$instances" | paste -sd,) +router=greedy,flow,greedy+ls,flow+ls,sga
