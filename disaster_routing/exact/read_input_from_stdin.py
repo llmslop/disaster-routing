d26f035c9cf5cd3ee27f1993550e1e748d266fcb
btmxh <ngoduyanh.chip@gmail.com>
2025-07-15 20:30:48 +0700

    feat(exact): allow reading graph from instance files

--- disaster_routing/exact/read_input_from_stdin.py
+++ disaster_routing/exact/read_input_from_stdin.py
@@ -1,9 +1,22 @@
+import json
+from typing import IO
 import pandas as pd
 import sys
 
+from disaster_routing.instances.instance import Instance
 
-def read_input_from_stdin():
-    excel = pd.ExcelFile(sys.stdin.buffer)
+import random
+
+# remove stochasticity
+random.seed(42)
+
+
+def read_input_from_stdin_xlsx(
+    inp: IO[bytes] = sys.stdin.buffer,
+) -> tuple[
+    list[int], list[tuple[int, int, int]], list[int], list[list[tuple[int, int, int]]]
+]:
+    excel = pd.ExcelFile(inp)
     df_nodes = pd.read_excel(excel, sheet_name="Nodes")
     df_edges = pd.read_excel(excel, sheet_name="Edges")
     df_datacenter = pd.read_excel(excel, sheet_name="Datacenters")
@@ -15,8 +28,8 @@ def read_input_from_stdin():
     # Tập hợp A (Danh sách Liên kết hợp lệ)
     A = []
     for _, row in df_edges.iterrows():
-        edge = (row["Source"], row["Target"], row["Weight"])
-        reverse_edge = (row["Target"], row["Source"], row["Weight"])
+        edge = (int(row["Source"]), int(row["Target"]), int(row["Weight"]))
+        reverse_edge = (int(row["Target"]), int(row["Source"]), int(row["Weight"]))
 
         A.append(edge)
         A.append(reverse_edge)  # Thêm cạnh đảo ngược
@@ -38,19 +51,56 @@ def read_input_from_stdin():
         ]
 
         if affected_edges:
+            affected_edges.sort()
             Z.append(affected_edges)
 
-    # Kết quả
-    print("\nTập hợp V (Danh sách Nodes):")
-    print(V)
-
-    print("\nTập hợp A (Danh sách Liên kết):")
-    print(A)
-
-    print("\nTập hợp D (Danh sách Datacenter - Nodes Hình Vuông):")
-    print(D)
-
-    print("\nTập hợp Z (Danh sách Deadzones - Nhóm Liên kết bị ảnh hưởng):")
-    print(Z)
-
+    A.sort()
+    Z.sort()
     return V, A, D, Z
+
+
+def read_input_from_stdin_inst(
+    inp: IO[bytes] = sys.stdin.buffer,
+) -> tuple[
+    list[int], list[tuple[int, int, int]], list[int], list[list[tuple[int, int, int]]]
+]:
+    inst = Instance.from_json(json.load(inp))
+    top = inst.topology
+    graph = top.graph
+
+    V = sorted(v for v in graph.nodes)
+    A: list[tuple[int, int, int]] = []
+    for u, v in graph.edges:
+        A.append((u, v, graph.edges[u, v]["weight"]))
+    D = [2, 5, 9]  # TODO: make this part of the instance maybe?
+    Z: list[list[tuple[int, int, int]]] = []
+    for dz in top.dzs:
+        affected_edges = [e for e in A if e[0] in dz.nodes or e[1] in dz.nodes]
+        if affected_edges:
+            affected_edges.sort()
+            Z.append(affected_edges)
+    A.sort()
+    Z.sort()
+    return V, A, D, Z
+
+
+if __name__ == "__main__":
+    with open("res/Graph/NSFNET.xlsx", "rb") as f:
+        nsfnet_1 = read_input_from_stdin_xlsx(f)
+    with open("instances/nsfnet-0010-01.json", "rb") as f:
+        nsfnet_2 = read_input_from_stdin_inst(f)
+
+    n1 = json.dumps(nsfnet_1, sort_keys=True)
+    n2 = json.dumps(nsfnet_2, sort_keys=True)
+
+    print("NSFNET from XLSX:")
+    print(n1)
+    print("NSFNET from JSON instance:")
+    print(n2)
+
+    if n1 != n2:
+        print("Different representations!")
+    else:
+        print("NSFNET repsresentations are the same!")
+
+read_input_from_stdin = read_input_from_stdin_inst
