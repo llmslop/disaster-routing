eff00ff5d85f51ddbc853d277ce2ba826b7bb81c
btmxh <ngoduyanh.chip@gmail.com>
2025-07-21 08:52:10 +0700

    feat(scripts): allow custom args in `run_all.sh`

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 instances=$(find instances)
-if [ -n "$1" ]; then
-    instances=$(echo "$instances" | grep -E $1)
+pattern=""
+extra_args=""
+
+for arg in "$@"; do
+    case $arg in
+        --pattern=*)
+            pattern="${arg#*=}"
+            ;;
+        *)
+            extra_args="$extra_args $arg"
+            ;;
+    esac
+done
+
+if [ -n "$pattern" ]; then
+    instances=$(echo "$instances" | grep -E "$pattern")
 fi
 
-uv run -m disaster_routing.main -m instance.path=$(echo "$instances" | paste -sd,) +router=greedy,flow,greedy+ls,flow+ls,sga
+uv run -m disaster_routing.main -m instance.path=$(echo "$instances" | paste -sd,) $extra_args
